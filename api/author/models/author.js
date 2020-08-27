a44321a73347ed54fb06c89cf830f5e5f3a75af3
Alejandro <aleperaltabazas@hotmail.com>
2020-08-27 16:44:10 -0300

    simplifico relaciones entre proyectos y usuarios para consistencia mas facil

--- api/author/models/author.js
+++ api/author/models/author.js
@@ -5,15 +5,4 @@
  * to customize this model
  */
 
-module.exports = {
-  lifecycles: {
-    async afterCreate(data) {
-      strapi.query("author-ref").create({
-        username: data.username,
-        full_name: data.full_name,
-        profile_url: `users/${data.id}`,
-        user_id: data.id,
-      });
-    },
-  },
-};
+module.exports = {};
--- api/project/models/project.js
+++ api/project/models/project.js
@@ -5,48 +5,4 @@
  * to customize this model
  */
 
-function createProjectRef(data) {
-  return strapi
-    .query("project-ref")
-    .create({ project_id: data.id, project_name: data.title });
-}
-
-function addProjectToUser(authorRef, projectRef) {
-  return strapi
-    .query("author")
-    .find({ id: authorRef.user_id })
-    .then((author) =>
-      strapi.query("author").update(
-        { id: authorRef.user_id },
-        {
-          project_refs: (author.project_refs ? author.project_refs : []).concat(
-            projectRef
-          ),
-        }
-      )
-    )
-    .catch(console.error);
-}
-
-function findProjectRef(projectId) {
-  return strapi.query("project-ref").find({ project_id: projectId });
-}
-
-module.exports = {
-  lifecycles: {
-    async afterCreate(data) {
-      createProjectRef(data)
-        .then((ref) =>
-          Promise.all(data.author_refs.map((a) => addProjectToUser(a, ref)))
-        )
-        .catch(console.error);
-    },
-    async afterUpdate(data) {
-      findProjectRef(data.id)
-        .then((ref) =>
-          Promise.all(data.author_refs.map((a) => addProjectToUser(a, ref)))
-        )
-        .catch(console.error);
-    },
-  },
-};
+module.exports = {};
