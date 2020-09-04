7826afe1f022bf8b48d2b3e1d792105a1452c46a
Alejandro <aleperaltabazas@hotmail.com>
2020-09-04 20:28:54 -0300

    Agrego endpoints para agregar y borrar autores y supervisores de un proyecto

--- api/project/controllers/project.js
+++ api/project/controllers/project.js
@@ -35,4 +35,48 @@ module.exports = {
       }
     );
   },
+  createAuthor: async (ctx) => {
+    const author = await strapi
+      .query("author")
+      .find({ _id: ctx.request.body.author_id });
+
+    return strapi
+      .query("project")
+      .update(
+        { _id: ctx.params.project_id },
+        { authors: project.authors.concat(author) }
+      );
+  },
+  createSupervisor: async (ctx) => {
+    const supervisor = await strapi
+      .query("supervisor")
+      .find({ _id: ctx.request.body.supervisor_id });
+
+    return strapi
+      .query("project")
+      .update(
+        { _id: ctx.params.project_id },
+        { authors: project.supervisors.concat(supervisor) }
+      );
+  },
+  deleteAuthor: async (ctx) => {
+    return strapi
+      .query("project")
+      .update(
+        { _id: ctx.params.project_id },
+        { authors: project.authors.filter((a) => a.id != ctx.params.author_id) }
+      );
+  },
+  deleteSupervisor: async (ctx) => {
+    return strapi
+      .query("project")
+      .update(
+        { _id: ctx.params.project_id },
+        {
+          supervisor_id: project.supervisors.filter(
+            (s) => s.id != ctx.params.supervisor_id
+          ),
+        }
+      );
+  },
 };
