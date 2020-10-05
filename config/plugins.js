e195306b2fed7c0478a66a98b384c26922b0aa01
matigiorda <matiasgiorda27@gmail.com>
2020-10-05 13:42:30 -0300

    genera yarn.lock

--- config/plugins.js
+++ config/plugins.js
@@ -1,3 +1,20 @@
 module.exports = ({ env }) => ({
-
+  upload: {
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: env("CLOUDINARY_NAME", process.env.cloud_name),
+      api_key: env("CLOUDINARY_KEY", process.env.api_key),
+      api_secret: env("CLOUDINARY_SECRET", process.env.api_secret),
+    },
+  },
+  email: {
+    provider: "sendgrid",
+    providerOptions: {
+      apiKey: env("SENDGRID_API_KEY", "${process.env.SENDGRID_API_KEY}"),
+    },
+    settings: {
+      defaultFrom: "proyeception@gmail.com",
+      defaultReplyTo: "proyeception@gmail.com",
+    },
+  },
 });
