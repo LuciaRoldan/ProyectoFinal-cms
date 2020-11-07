dcf327e5c9025c578d4087d192d24ac2ba226dd3
Ro <rociochipian@gmail.com>
2020-11-07 18:25:11 -0300

    remuevo el contador de views

--- api/project/models/project.js
+++ api/project/models/project.js
@@ -4,13 +4,3 @@
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
-
-module.exports = {
-  lifecycles: {
-    async afterFindOne(result, params, populate) {
-      strapi
-        .query("project")
-        .update({ _id: result.id }, { views: result.views + 1 });
-    },
-  },
-};
